291ba07fcd765de29546031dc63ddcdec7469b7a
Josie <jwyrick1999@gmail.com>
2021-11-15 22:22:58 -0600

    Initial Commit

---
+++ VocabMonsta/VocabMonstaApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocabMonstaApp.swift
+//  VocabMonsta
+//
+//  Created by Jocelyn Wyrick on 11/15/21.
+//
+
+import SwiftUI
+
+@main
+struct VocabMonstaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
